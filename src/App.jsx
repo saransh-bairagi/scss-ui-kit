2bfc52ee75fd86cd550626d5c586e602612967ac
saransh <saranshbairagi941@gmail.com>
2024-12-26 22:52:38 +0530

    Added Doc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import "./styleSheets/scss/main.scss"
-function App() {
-  return (
-    <div>Components Testing</div>
-  )
-}
+import React from "react";
+// importing Modules
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-export default App
+// importing Pages
+import HomePage from "./pages/HomePage.pages";
+import ErrorPage from "./pages/ErrorPage.pages";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Homepage */}
+                <Route path="/" element={<HomePage />} />
+                <Route path="/*" element={<ErrorPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
---
+++ src/pages/HomePage.pages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../styleSheets/scss/main.scss"
+function HomePage() {
+  return (
+    <div className='bg-red screen-h'>Welcome to Home</div>
+  )
+}
+
+export default HomePage
